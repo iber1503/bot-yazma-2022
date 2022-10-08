4cb7c01336fe2f092bb5c64755273def9bf34e26
iber1503 <ibrahimeren15@hotmail.com>
2022-10-08 11:53:57 +0300

    matematiksel
    matematiksel operatörler

---
+++ matematiksel_opeartorler.py
@@ -0,0 +1,19 @@
+"""
+
+#TOPLAMA: +
+#ÇIKARMA -
+#ÇARPMA  *
+#BÖLME   /
+#ÜS       **
+#MOD     % bölme işleminde kalan
+#TAM BÖLME // tam bölünmeyenlerde virgülden sonraları atıyor ondalıkları yani
+"""
+
+print(12+40)
+print(580-81)
+print (53*48)
+print(78/6)
+print(25**32)
+print(2**3**4)
+print(40%5) #0
+print(40//3) #13
