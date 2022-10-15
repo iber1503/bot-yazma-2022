14575db1ab13dd67d2cd219430db8a385a0dcdde
iber1503 <ibrahimeren15@hotmail.com>
2022-10-15 11:35:17 +0300

    karakter dizileri
    karakter dizileri

---
+++ karakter_dizileri.py
@@ -0,0 +1,16 @@
+#Kullanıcı yaş ve isim değişkenlerini oluşturalım
+yas="17"
+isim="Eren"
+
+print("Adınız:" +isim+",Yaşınız:"+str(yas))
+
+#karakter verisinin uzunluğuna bakalım
+
+print(len(isim))
+
+#str veri tipindeki değerin çeşitli elemanlarını yazdıralım
+#elemanlar 0'dan başlar
+print(isim[0])
+print(isim[1])
+print(isim[2])
+print(isim[3])
