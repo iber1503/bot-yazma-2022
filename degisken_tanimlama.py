c873d5a0d2e5525a486cb9a8dcc00d4d518110ca
iber1503 <ibrahimeren15@hotmail.com>
2022-10-15 11:33:44 +0300

    Değişken tanımlama
    değişken

---
+++ degisken_tanimlama.py
@@ -0,0 +1,45 @@
+# Değişkenler olmadan 2 öğrencinin sınav notlarını hesaplayalım
+# print( (80*0.4) + (75*0.6) )
+# print( (45*0.4) + (60*0.6) )
+
+#Değişkenlerle 2 öğrencinin sınav notlarını hesaplayalım
+sinav1_yuzde = 0.5
+sinav2_yuzde = 0.5
+
+#print ((80*sinav1_yuzde)+(75*sinav2_yuzde))
+#print ((45*sinav1_yuzde)+(60*sinav2_yuzde))
+
+#DEĞİŞKEN TANIMLAMA KURALLARI
+
+#Rakam ile başlayamaz
+#1sayi= 85 #Hata Verdi
+
+sayi1=85
+#print(sayi1)
+
+#Büyük Küçük Harfe Duyarlıdır
+number=12
+NUMBER=15
+print(number)
+print(NUMBER)
+#Türkçe Karakter KUllanılamaz
+#yaş=18 hatalı
+yas =18 # Türkçe karakter olmadan türkçe yazmak (tavsiye edilmez)
+age= 17
+print(yas)
+print(age)
+
+#Değişenlerdeki Verileri Birleştirme
+
+x=1 #int
+y=1.2 #float
+ad="Eren" #string
+sinav_basarili_mi= True #boolean
+
+print(x+y)
+#print(x+ad) hata verdi
+print(x+sinav_basarili_mi)
+print(ad+sinav_basarili_mi)
+#print(ad+sinav_basarili_mi) hata verdi
+
+
