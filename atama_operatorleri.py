1487e810e949daea831d3982ccddfb848f2b8e72
İbrahim Eren <ibrahimeren15@hotmail.com>
2022-11-26 11:07:41 +0300

    atama operatorleri dersi

---
+++ atama_operatorleri.py
@@ -0,0 +1,49 @@
+"""
+ATAMA OPERATÖRLERİ
+//////////////////////////////////////////////////////////////////////////////
+Python'da değişkenlere veri ataması yaparken kullanırız.
++----------+-----------+----------------+
+| Operatör | Kullanımı | Uzun Kullanımı |
++----------+-----------+----------------+
+| =        | x = y     |                |
++----------+-----------+----------------+
+| +=       | x += y    | x = x + y      |
++----------+-----------+----------------+
+| -=       | x -= y    | x = x - y      |
++----------+-----------+----------------+
+| *=       | x *= y    | x = x * y      |
++----------+-----------+----------------+
+| /=       | x /= y    | x = x / y      |
++----------+-----------+----------------+
+| %=       | x %= y    | x = x % y      |
++----------+-----------+----------------+
+| **=      | x **= y   | x = x ** y     |
++----------+-----------+----------------+
+| //=      | x //= y   | x = x // y     |
++----------+-----------+----------------+
+"""
+
+#sayısal verilere sahip 3 değişken oluşturalım
+a=2
+b=3
+c=4
+
+
+#üç değişkeni tek satırda tanımlayalım
+x,y,z = 5,6,7
+print(x,y,z)
+
+#deişkendeki verileri yer değiştirelim
+x,y,z= y,z,x
+print(x,y,z)
+
+#değişken içindeki değere 10 ekleyelim
+x=x+10 #x artık 16
+y+=10 #y artık 17
+print(x,y)
+
+#x ve y değişkenlerinde değeri 5 azaltalım (x:uzun yol,y:kısa yol)
+
+x= x-5
+y-=5
+print(x,y)
